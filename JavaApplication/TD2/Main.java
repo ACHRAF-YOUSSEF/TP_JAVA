6802060d88db82a24a29e3f8fe14ee4a86d74f65
Achraf-Youssef <100535820+Achraf-Youssef@users.noreply.github.com>
2022-11-23 23:27:37 +0100

    Update Main.java

--- JavaApplication/TD2/Main.java
+++ JavaApplication/TD2/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         EtudiantDoctorat etudiant1 = new EtudiantDoctorat("achraf youssef", "sousse", 1, "");
         EtudiantMaster etudiant2 = new EtudiantMaster("amine hattab", "sousse", 5, "");
-        EtudiantLicence etudiant3 = new EtudiantLicence("islam rabhi", "sousse", 6, "");
+        EtudiantLicence etudiant3 = new EtudiantLicence("islam rabhi", "sousse", 6, "info");
 
         System.out.println("les etudiants:\n");
         etudiant1.afficherProfil();
